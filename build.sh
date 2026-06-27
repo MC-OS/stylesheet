6e5552212bf8f55388c3b2db84fbf665cae0f028
DJABHipHop <castellanos.kevin@rocketmail.com>
2026-06-26 23:20:08 -0400

    Update build script to copy to all locations aster build is done

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ cd build/
 sudo ninja
 sudo ninja install
 cd ../
+cp -r /usr/local/share/themes/io.elementary.stylesheet.* ~/.themes/
+sudo cp -r /usr/local/share/themes/io.elementary.stylesheet.* /usr/share/themes/
